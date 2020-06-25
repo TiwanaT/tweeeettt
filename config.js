3e0da3815c7846228d99bfe56854b13f96b1e244
TiwanaT <61065718+TiwanaT@users.noreply.github.com>
2020-06-24 20:32:46 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  consumer_key: '##',
-  consumer_secret: '##',
-  access_token_key: '##',
-  access_token_secret: '##',
+  consumer_key: '#j8YJSgpN3Rc1SpA9Sa17S15rl#',
+  consumer_secret: '#qDV8WXLBYsJ6zxW2SXHxNo0cHMrFnKZSrhVSYb4IJGnn6fAOsq#',
+  access_token_key: '#832317068-sJVn7HGRAjGXZQ4ESYjokCRRlQSVK975DJ1fjayv#',
+  access_token_secret: '#67iXED3MiipP76BPdCHzxyzjHcS1uzV1WscHSBiF7KTdv#',
   path: './tweet.js', // path to the archive tweet.js file
-  maxDate: false, // delete older than this eg: '2011-12-31 00:00:00 +0000'
+  maxDate: '2018-08-01 12:00:00', // delete older than this eg: '2011-12-31 00:00:00 +0000'
   saveRegexp: [], // save tweets matching this regexp: eg ['#newavatar', '@\w+']
   callsInterval: 500 // ms
 }
